8c7a93adc6053ba0a094d60d60a090b33b2bce71
akash <akash.ry@somaiya.edu>
2021-08-17 13:45:02 +0530

    react starter template

--- src/components/User.js
+++
@@ -1,9 +0,0 @@
-import React, { Component } from 'react'
-
-class User extends Component {
-    render() {
-        return <h2>{this.props.render(false)}</h2>
-    }
-}
-
-export default User
--- src/components/userContext.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-const UserContext = React.createContext("AkuSky")
-
-const UserProvider = UserContext.Provider
-const UserConsumer = UserContext.Consumer
-
-export { UserProvider, UserConsumer}
-export default UserContext
